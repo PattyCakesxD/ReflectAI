ccc5b04c8e8cf34f7c9336ca2a1e37ee7a8ce042
PattyCakesxD <nguyenpatty42@gmail.com>
2025-05-21 01:26:30 -0700

    setup file upload button

--- main.py
+++ main.py
@@ -6,5 +6,14 @@ from openai import OpenAI
 from dotenv import load_dotenv
 
 load_dotenv()
-
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
+st.set_page_config(page_title="ReflectAI", layout="centered")
+
+st.markdown("""
+            # :blue[ResumeAI]
+
+            #### See your resume through a *:rainbow[critical eye]*
+            """)
+
+uploaded_file = st.file_uploader(":green[Upload your resume] :gray[(PDF)]", type=["pdf"])
